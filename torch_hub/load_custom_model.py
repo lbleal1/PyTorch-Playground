a2c6c26d552c6c0935e189dd4b25150d8e2407b5
lbleal1 <lowwwiis@gmail.com>
2023-03-20 15:05:01 +0000

    model moved

--- torch_hub/load_custom_model.py
+++ torch_hub/load_custom_model.py
@@ -3,5 +3,7 @@ import torch
 # for the HTTP Error 403
 torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
 
-model = torch.hub.load('lbleal1/pytorch-playground', 'custom_model')
+model = torch.hub.load('lbleal1/pytorch-playground', 
+                       'custom_model',
+                       trust_repo = True)
 print(model)
