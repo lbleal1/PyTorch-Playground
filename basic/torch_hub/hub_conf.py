1548437fc744e914ff306f1047d8cbac8575bb10
lbleal1 <lowwwiis@gmail.com>
2023-03-20 14:55:47 +0000

    torch hub

---
+++ basic/torch_hub/hub_conf.py
@@ -0,0 +1,12 @@
+import torch
+from model import Model # imported model class
+
+def custom_model():
+    """ # This docstring shows up in hub.help()
+    Loads a model from a path
+    and returns the model 
+    """
+    # load
+    path = 'iris_classifier.pt'
+    model = torch.load(path)
+    return model
